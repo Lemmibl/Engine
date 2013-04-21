edc6c906059e98f9748f2e8fb552f7eb55d735b5
Carl Engström <Lemmibl@gmail.com>
2013-04-21 22:37:13 +0200

    Tweaked shadow code for vegetation slightly.. It's.. being blocky.

--- Engine/DepthOnlyQuadShader.cpp
+++ Engine/DepthOnlyQuadShader.cpp
@@ -191,12 +191,12 @@ bool DepthOnlyQuadShader::InitializeShader(ID3D11Device* device, HWND hwnd, WCHA
 	}
 
 	// Create a texture sampler state description.
-	samplerDesc.Filter = D3D11_FILTER_ANISOTROPIC;
+	samplerDesc.Filter = D3D11_FILTER_COMPARISON_MIN_MAG_MIP_LINEAR;
 	samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_CLAMP;
 	samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_CLAMP;
 	samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_CLAMP;
 	samplerDesc.MipLODBias = 0.0f;
-	samplerDesc.MaxAnisotropy = 16;
+	samplerDesc.MaxAnisotropy = 1;
 	samplerDesc.ComparisonFunc = D3D11_COMPARISON_ALWAYS;
 	samplerDesc.BorderColor[0] = 0;
 	samplerDesc.BorderColor[1] = 0;
